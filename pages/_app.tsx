d3f6bd6da95cbcbc8a1c08bce254672faa9527a0
Łukasz Jakubowski <lja@spyro-soft.com>
2023-08-21 19:13:23 +0200

    09-devtools: Install and initiate react query devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
       <ToastContainer position="bottom-left" />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
