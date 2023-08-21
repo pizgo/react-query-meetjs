019f8354cef5b4190ae307affe712f4c4524d466
Łukasz Jakubowski <lja@spyro-soft.com>
2023-08-21 17:37:43 +0200

    04-get-all-data-by-use-query: useQuery for fetching all restaurants

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import { QueryKeysRestaurantsEnum } from "models/query-keys-models";
 import type { NextPage } from "next";
+import { getRestaurants } from "services/get-restaurants";
 
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
-  return <section className={styles.container}>Home</section>;
+  const {
+    data: restaurantsList,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: [QueryKeysRestaurantsEnum.restautants],
+    queryFn: getRestaurants,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  return (
+    <section className={styles.container}>
+      <ul className={styles["main-list"]}>
+        {restaurantsList?.restaurants.map((restaurant) => {
+          return (
+            <li className={styles["main-list-item"]} key={restaurant.id}>
+              <b>{restaurant.name}</b> - {restaurant.address}
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 };
 
 export default Home;
